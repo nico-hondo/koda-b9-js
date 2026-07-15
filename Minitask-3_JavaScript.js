4567274ae56838ed97dc3418cfd6ae5aba3b1d7a
Nico Fernando Hondo <62182543+nico-hondo@users.noreply.github.com>
2026-07-15 15:46:32 +0700

    Add files via upload

---
+++ Minitask-3_JavaScript.js
@@ -0,0 +1,33 @@
+const totalBeli = 27000
+const bayar = 100000;
+
+const kembalian = bayar - totalBeli;
+let decrease = kembalian;
+decrease = Number(decrease);
+let hitung = '';
+
+while (decrease > 0) {
+    if( decrease >= 50000){
+        hitung += '50000, ';
+        decrease -= 50000;
+    }else if(decrease >= 10000){
+        hitung += '10000, ';
+        decrease -= 10000;
+    }else if(decrease >= 5000){
+        hitung += '5000, ';
+        decrease -= 5000;
+    }else if(decrease >= 2000){
+        hitung += '2000, ';
+        decrease -= 2000;
+    }else if(decrease >= 1000){
+        hitung += '1000, ';
+        decrease -= 1000;
+    }else{
+        break
+    }
+}
+
+console.log(kembalian);
+console.log(`Kembalian: ${hitung}`);
+
+//susah pakai for :(
