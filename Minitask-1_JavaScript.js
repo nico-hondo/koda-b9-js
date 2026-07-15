a28b1e09abcb69ca6ef4e3219fffed40808e6187
Nico Fernando Hondo <62182543+nico-hondo@users.noreply.github.com>
2026-07-15 11:52:09 +0700

    Add files via upload

---
+++ Minitask-1_JavaScript.js
@@ -0,0 +1,51 @@
+//Soal 1
+const who = {
+    am: {
+        i: "Nico"
+    }
+}
+console.log(who.am.i)
+
+
+//Soal 2
+const hello = {
+    world: "Hai Dunia Gembiralah"
+}
+console.log(hello.world)
+
+//Soal 3
+const obj = {
+    str:{
+        1: {
+            2:{
+                3:{
+                    koda:{
+                        0:{
+                            tech:{
+                                academy: "Koda Tech Academy"
+                            }
+                    
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+console.log(obj.str[1][2][3].koda[0].tech.academy)
+
+//soal 4
+const my = [
+    {
+        very : {
+            best:{
+                1:{
+                    sport:{
+                        is: "Football"
+                    }
+                }
+            }
+        }
+    }
+]
+console.log(my[0].very["best"][1].sport.is)
