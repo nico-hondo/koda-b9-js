3af9656b9ac5a787491adc50d65c8aeffa89a612
Nico Fernando Hondo <62182543+nico-hondo@users.noreply.github.com>
2026-07-16 18:23:24 +0700

    Add files via upload

---
+++ Minitask-5_JavaScript.js
@@ -0,0 +1,26 @@
+const account = {
+    id: 'acc1',
+    username : 'uxNicoFH',
+    pass : 'adaApaDengan@mu',
+};
+
+const updateAcc = {
+    pass : 'akunicosehatdankuat15',
+};
+let updateAccExe;
+
+//check apakah password baru mencapai 8 karakter
+const check = updateAcc.pass.length;
+
+// console.log(check);
+
+if(account instanceof Object){
+    if(check >= 8){
+        updateAccExe = {...account, ...updateAcc};
+        console.log(account, " --> ", updateAccExe)
+    }else{
+        console.log("Silahkan buat password minimal 8 karakter");
+    }
+}else{
+    console.log("Jangan nyusahin deh tim dev");
+}
